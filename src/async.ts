f8590d862a3f2b08c370a4c1403e6e6a810f931f
Brian Faust <hello@basecode.sh>
2019-05-15 05:45:53 +0300

    use @keeveestore/file to abstract in-memory store logic

--- src/async.ts
+++ src/async.ts
@@ -1,86 +1,8 @@
-import { IKeyValueStoreAsync } from "@keeveestore/keeveestore";
+import { StoreAsync as AbstractStore } from "@keeveestore/file";
 import { StoreSync } from "./sync";
 
-export class StoreAsync<K, T> implements IKeyValueStoreAsync<K, T> {
-	private constructor(private readonly store: StoreSync<K, T>) {}
-
+export class StoreAsync<K, T> extends AbstractStore<K, T> {
 	public static async new<K, T>(uri: string): Promise<StoreAsync<K, T>> {
 		return new StoreAsync<K, T>(StoreSync.new<K, T>(uri));
-	}
-
-	public async all(): Promise<[K, T][]> {
-		return this.store.all();
-	}
-
-	public async keys(): Promise<K[]> {
-		return this.store.keys();
-	}
-
-	public async values(): Promise<T[]> {
-		return this.store.values();
-	}
-
-	public async get(key: K): Promise<T | undefined> {
-		return this.store.get(key);
-	}
-
-	public async getMany(keys: K[]): Promise<(T | undefined)[]> {
-		return this.store.getMany(keys);
-	}
-
-	public async pull(key: K): Promise<T | undefined> {
-		return this.store.pull(key);
-	}
-
-	public async pullMany(keys: K[]): Promise<(T | undefined)[]> {
-		return this.store.pullMany(keys);
-	}
-
-	public async put(key: K, value: T): Promise<boolean> {
-		return this.store.put(key, value);
-	}
-
-	public async putMany(values: [K, T][]): Promise<boolean[]> {
-		return this.store.putMany(values);
-	}
-
-	public async has(key: K): Promise<boolean> {
-		return this.store.has(key);
-	}
-
-	public async hasMany(keys: K[]): Promise<boolean[]> {
-		return this.store.hasMany(keys);
-	}
-
-	public async missing(key: K): Promise<boolean> {
-		return this.store.missing(key);
-	}
-
-	public async missingMany(keys: K[]): Promise<boolean[]> {
-		return this.store.missingMany(keys);
-	}
-
-	public async forget(key: K): Promise<boolean> {
-		return this.store.forget(key);
-	}
-
-	public async forgetMany(keys: K[]): Promise<boolean[]> {
-		return this.store.forgetMany(keys);
-	}
-
-	public async flush(): Promise<boolean> {
-		return this.store.flush();
-	}
-
-	public async count(): Promise<number> {
-		return this.store.count();
-	}
-
-	public async isEmpty(): Promise<boolean> {
-		return this.store.isEmpty();
-	}
-
-	public async isNotEmpty(): Promise<boolean> {
-		return this.store.isNotEmpty();
 	}
 }
--- src/sync.ts
+++ src/sync.ts
@@ -1,11 +1,26 @@
 // tslint:disable: no-unsafe-any
-import { IKeyValueStoreSync } from "@keeveestore/keeveestore";
+import { StoreSync as AbstractStore } from "@keeveestore/file";
 import { readFileSync, writeFileSync } from "fs";
 import { ensureFileSync } from "fs-extra";
 import xml from "xml-js";
 
-export class StoreSync<K, T> implements IKeyValueStoreSync<K, T> {
-	private constructor(private readonly store: Map<K, T>, private readonly uri: string) {
+export class StoreSync<K, T> extends AbstractStore<K, T> {
+	public static new<K, T>(uri: string): StoreSync<K, T> {
+		return new StoreSync<K, T>(new Map<K, T>(), uri);
+	}
+
+	protected dump(): void {
+		// @ts-ignore
+		const rows: Record<K, T> = {};
+
+		for (const [key, value] of this.all()) {
+			rows[key] = value;
+		}
+
+		writeFileSync(this.uri, xml.js2xml(rows, { compact: true, spaces: 4 }));
+	}
+
+	protected load(): void {
 		ensureFileSync(this.uri);
 
 		const entries: xml.Element | xml.ElementCompact = xml.xml2js(readFileSync(this.uri, "utf8"));
@@ -15,116 +30,5 @@ export class StoreSync<K, T> implements IKeyValueStoreSync<K, T> {
 				this.put(entry.name, entry.elements[0].text);
 			}
 		}
-	}
-
-	public static new<K, T>(uri: string): StoreSync<K, T> {
-		return new StoreSync<K, T>(new Map<K, T>(), uri);
-	}
-
-	public all(): [K, T][] {
-		return [...this.store.entries()];
-	}
-
-	public keys(): K[] {
-		return [...this.store.keys()];
-	}
-
-	public values(): T[] {
-		return [...this.store.values()];
-	}
-
-	public get(key: K): T | undefined {
-		return this.store.get(key);
-	}
-
-	public getMany(keys: K[]): (T | undefined)[] {
-		return [...keys].map((key: K) => this.get(key));
-	}
-
-	public pull(key: K): T | undefined {
-		const item: T | undefined = this.get(key);
-
-		this.forget(key);
-
-		return item;
-	}
-
-	public pullMany(keys: K[]): (T | undefined)[] {
-		const items: (T | undefined)[] = this.getMany(keys);
-
-		this.forgetMany(keys);
-
-		return items;
-	}
-
-	public put(key: K, value: T): boolean {
-		this.store.set(key, value);
-
-		this.dump();
-
-		return this.has(key);
-	}
-
-	public putMany(values: [K, T][]): boolean[] {
-		return values.map((value: [K, T]) => this.put(value[0], value[1]));
-	}
-
-	public has(key: K): boolean {
-		return this.store.has(key);
-	}
-
-	public hasMany(keys: K[]): boolean[] {
-		return [...keys].map((key: K) => this.has(key));
-	}
-
-	public missing(key: K): boolean {
-		return !this.has(key);
-	}
-
-	public missingMany(keys: K[]): boolean[] {
-		return [...keys].map((key: K) => this.missing(key));
-	}
-
-	public forget(key: K): boolean {
-		const deleted: boolean = this.store.delete(key);
-
-		this.dump();
-
-		return deleted;
-	}
-
-	public forgetMany(keys: K[]): boolean[] {
-		return [...keys].map((key: K) => this.forget(key));
-	}
-
-	public flush(): boolean {
-		this.store.clear();
-
-		this.dump();
-
-		return this.count() === 0;
-	}
-
-	public count(): number {
-		return this.store.size;
-	}
-
-	public isEmpty(): boolean {
-		return this.count() === 0;
-	}
-
-	public isNotEmpty(): boolean {
-		return !this.isEmpty();
-	}
-
-	private dump(): void {
-		// @ts-ignore
-		const rows: Record<K, T> = {};
-
-		for (const [key, value] of this.all()) {
-			rows[key] = value;
-		}
-
-		writeFileSync(this.uri, xml.js2xml(rows, { compact: true, spaces: 4 }));
 	}
 }
