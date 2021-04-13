23a8ee159c266d2e46b0f75e38ba2f09638340d0
Brian Faust <hello@basecode.sh>
2021-04-13 16:56:09 +0300

    chore: integrate `@konceiver/tease`

--- src/sync.ts
+++ src/sync.ts
@@ -1,6 +1,6 @@
 // tslint:disable: no-unsafe-any
 import { StoreSync as AbstractStore } from "@konceiver/kv-file";
-import { readFileSync, writeFileSync } from "fs";
+import { ensureFileSync, readFileSync, writeFileSync } from "fs-extra";
 import xml from "xml-js";
 
 export class StoreSync<K, T> extends AbstractStore<K, T> {
@@ -8,18 +8,28 @@ export class StoreSync<K, T> extends AbstractStore<K, T> {
 		return new StoreSync<K, T>(new Map<K, T>(), uri);
 	}
 
-	// @ts-ignore
-	protected dump(rows: Record<K, T>): void {
-		writeFileSync(this.uri, xml.js2xml(rows, { compact: true, spaces: 4 }));
+	protected dump(): void {
+		writeFileSync(
+			this.uri,
+			xml.js2xml(this.all(), { compact: true, spaces: 4 })
+		);
 	}
 
 	protected load(): void {
-		const entries: xml.Element | xml.ElementCompact = xml.xml2js(readFileSync(this.uri, "utf8"));
+		ensureFileSync(this.uri);
 
-		if (entries.elements) {
-			for (const entry of entries.elements) {
-				this.put(entry.name, entry.elements[0].text);
+		try {
+			const entries: xml.Element | xml.ElementCompact = xml.xml2js(
+				readFileSync(this.uri, "utf8")
+			);
+
+			if (entries.elements) {
+				for (const entry of entries.elements) {
+					this.put(entry.name, entry.elements[0].text);
+				}
 			}
+		} catch {
+			//
 		}
 	}
 }
